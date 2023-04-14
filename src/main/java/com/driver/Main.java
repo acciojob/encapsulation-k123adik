a2f4d19152c38d1fcce55cf4b24a35ee11f7bb6c
Aditya Kshirsagar <Aditya Kshirsagar@aditya.aditya>
2023-04-14 16:42:41 +0530

    All task done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+          RWOnly obj = new RWOnly();
+
+          obj.setName("Aditya");
+          String ans = obj.getName();
+
+        System.out.println(ans);
+    }
   
 }
